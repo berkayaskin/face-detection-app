e38d387025a6d5923d41c9a5054e650758111b9e
Berkay Askin <berkayaskiin@gmail.com>
2018-09-01 14:18:44 +0000

    add functionality to face detection box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,11 +3,12 @@ import './FaceRecognition.css';
 
  
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({imageUrl, box}) => {
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
-                <img alt='' src= {imageUrl} width='700px' height='auto' />
+                <img id='inputimage' alt='' src= {imageUrl} width='700px' height='auto' />
+                <div className='bounding_box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
             </div>
         </div>
     );
