64b61ba5dff78944bf5493560fcbebc5ab0d4025
berkayaskin <https://github.com/berkayaskin>
2018-09-06 14:42:16 +0300

    create registration of users

--- src/App.js
+++ src/App.js
@@ -35,10 +35,27 @@ class App extends Component {
       imageUrl:'',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      users: {
+        id:'',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     };
   }
   
+  loadUser = (data) => {
+    this.setState ({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
@@ -99,7 +116,7 @@ class App extends Component {
             : (
                 route === 'signin' 
                 ? <Signin onRouteChange={this.onRouteChange} />
-                : <Register onRouteChange={this.onRouteChange} />
+                : <Register  loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
               )
           }
       </div>
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -46,11 +46,23 @@ class Signin extends React.Component {
                 <legend className="f2 fw6 ph0 mh0">Sign In</legend>
                 <div className="mt3">
                   <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                  <input onChange={this.onEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                  <input 
+                    onChange={this.onEmailChange} 
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                    type="email" 
+                    name="email-address"  
+                    id="email-address" 
+                  />
                 </div>
                 <div className="mv3">
                   <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                  <input onChange={this.onPasswordChange} className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                  <input 
+                    onChange={this.onPasswordChange} 
+                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                    type="password" 
+                    name="password"  
+                    id="password" 
+                  />
                 </div>
               </fieldset>
               <div className="">
